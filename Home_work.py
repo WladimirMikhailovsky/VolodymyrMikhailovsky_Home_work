108b0625614e4b626e363e6a8a111109996bacb6
Vladimir <mikhailovskyvolodymyr@gmail.com>
2018-01-04 17:05:59 +0200

    Addition (tasks 1 - 10)

--- Home_work.py
+++ Home_work.py
@@ -62,11 +62,12 @@ print(european_date_style)
 # Еще один вариант кода:
 
 current_date = "05.17.2016"
-split = current_date.split('.')
-months = int(split[0])
-days = int(split[1])
-years = int(split[2])
+data_1st = current_date.split('.')
+months = int(data_1st[0])
+days = int(data_1st[1])
+years = int(data_1st[2])
 print("%d.%d.%d" % (days, months, years))
+print("%s.%s.%s" % (data_1st[1], data_1st[0], data_1st[2]))
 print("--------------------------------------------")
 
 #8.	Дана строка с именем студента, в которой имя предшествует фамилии, например
@@ -74,9 +75,9 @@ print("--------------------------------------------")
 # фамилию на первое место, а имя на второе. Т.е. ‘Mark Zuckerberg‘ -> ‘Zuckerberg Mark‘.
 
 name_surname = "Mark Zuckerberg"
-split = name_surname.split(" ")
-name = split[0]
-surname = split[1]
+data_1st = name_surname.split(" ")
+name = data_1st[0]
+surname = data_1st[1]
 print("Задание 8")
 print("%s %s" % (surname, name))
 print("--------------------------------------------")
@@ -88,9 +89,9 @@ print("--------------------------------------------")
 print("Задание 9")
 snake_style_name = "employee_first_name"
 split1 = snake_style_name.split("_")
-CamelizedStyleName = split1[0].capitalize() + split1[1].capitalize() + split1[2].capitalize()
+camelized_style_name = split1[0].capitalize() + split1[1].capitalize() + split1[2].capitalize()
 print("Snake_style: %s" % snake_style_name)
-print("CamelizedStyle: %s" % CamelizedStyleName)
+print("CamelizedStyle: %s" % camelized_style_name)
 print("--------------------------------------------")
 
 #10.Дана строка вида 'Leo Tolstoy*1828-08-28*1910-11-20'. В этой строке указаны имя
@@ -117,6 +118,41 @@ year_old = int(life_dates[3]) - int(life_dates[0])
 name_year = info_split[0] + " - " + str(year_old)
 print("Имя и возраст художника: %s" % (name_year))
 print("--------------------------------------------")
+
+#11.Написать функцию, которая будет переводить градусы в радианы.
+# Используя эту функцию вывести на экран значения косинусов углов в 60, 45 и 40 градусов.
+
+print("Задание 11")
+def degrees2radians(degrees):
+    return float((degrees * math.pi) / 180)
+print(degrees2radians(60))
+print(degrees2radians(45))
+print(degrees2radians(40))
+print("--------------------------------------------")
+
+#13.Пользователь вводит длины катетов прямоугольного треугольника.
+# Написать функцию, которая вычислит площадь треугольника и его периметр.
+# Результат работы функции вывести на печать.
+
+print("Задание 13")
+def triangle_square_and_perimeter(a, b):
+    return float((a * b) / 2) # Triangle square
+    return float(math.sqrt((a**2 + b**2))) # Hypotenuse
+    return float(a + b + c) # Triangle_perimeter
+print(triangle_square_and_perimeter(3, 2))
+print("--------------------------------------------")
+
+#14.Написать функцию, которая будет проверять четность некоторого числа.
+
+print("Задание 14")
+def is_even(number):
+    return number % 2 == 0
+print(is_even(3))
+print("--------------------------------------------")
+
+
+
+
 
 
 
